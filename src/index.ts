2d9839d2b8e9ceeb493a8c0f027f127796704c9a
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2023-11-05 17:03:56 +0800

    refactor: remove useless code for serverless

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,3 @@ export const bot = new Telegraf(BOT_TOKEN)
 
 bot.start((ctx) => ctx.reply('Welcome!'))
 bot.help((ctx) => ctx.reply('Help text...'))
-
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))

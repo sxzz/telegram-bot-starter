0fb29283fa74599051a6c3b208efd6feb9223818
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-05-12 01:12:01 +0800

    chore: cleanup eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,3 @@
 import { sxzz } from '@sxzz/eslint-config'
-export default sxzz([
-  {
-    ignores: ['**/.nitro'],
-  },
-  {
-    files: ['routes/*.ts'],
-    rules: {
-      'import/no-default-export': 'off',
-    },
-  },
-])
+
+export default sxzz()

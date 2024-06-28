bb0976ba25ab38d15a9017bd543f35abb241a32a
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-06-29 06:02:50 +0800

    refactor: extract set-webhook

---
+++ routes/set-webhook.ts
@@ -0,0 +1,20 @@
+import process from 'node:process'
+import { bot } from '../src'
+
+const SECRET_HASH = process.env.SECRET_HASH!
+
+export default eventHandler(async (evt) => {
+  const query = getQuery(evt)
+  if (query.secret_hash !== SECRET_HASH) {
+    return 'Forbidden'
+  }
+
+  const host = getRequestHeader(evt, 'x-forwarded-host') || getRequestHost(evt)
+  const webhookUrl = `https://${host}/telegram-hook?secret_hash=${SECRET_HASH}`
+  const isSet = await bot.telegram.setWebhook(webhookUrl)
+  const info = await bot.telegram.getWebhookInfo()
+  return `Set webhook to ${webhookUrl.replaceAll(
+    SECRET_HASH,
+    '*',
+  )}: ${isSet}<br/>${JSON.stringify(info).replaceAll(SECRET_HASH, '*')}`
+})
--- routes/telegram-hook.ts
+++ routes/telegram-hook.ts
@@ -1,28 +1,15 @@
 import process from 'node:process'
 import { bot } from '../src'
 
-const SECRET_HASH = process.env.SECRET_HASH
+const SECRET_HASH = process.env.SECRET_HASH!
 
 export default eventHandler(async (evt) => {
   const query = getQuery(evt)
-
-  if (query.setWebhook === 'true') {
-    const host =
-      getRequestHeader(evt, 'x-forwarded-host') || getRequestHost(evt)
-    const webhookUrl = `https://${host}/telegram-hook?secret_hash=${SECRET_HASH}`
-    const isSet = await bot.telegram.setWebhook(webhookUrl)
-    const info = await bot.telegram.getWebhookInfo()
-    return `Set webhook to ${webhookUrl.replaceAll(
-      SECRET_HASH,
-      "*"
-    )}: ${isSet}<br/>${JSON.stringify(info).replaceAll(
-      SECRET_HASH,
-      "*"
-    )}`
-  } else if (query.secret_hash === SECRET_HASH) {
-    const body = await readBody(evt)
-    await bot.handleUpdate(body)
+  if (query.secret_hash !== SECRET_HASH) {
+    return 'Forbidden'
   }
 
-  return 'Forbidden'
+  const body = await readBody(evt)
+  await bot.handleUpdate(body)
+  return 'OK'
 })

4d5453dc33d3b602f025f67cd42755636b7ba332
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-20 22:49:17 +0800

    refactor: remove token in url

--- routes/set-webhook.ts
+++ routes/set-webhook.ts
@@ -1,15 +1,12 @@
-import process from 'node:process'
 import { bot } from '../src'
-
-const TOKEN = process.env.TOKEN!
 
 export default eventHandler(async (evt) => {
   const host = getRequestHeader(evt, 'x-forwarded-host') || getRequestHost(evt)
-  const webhookUrl = `https://${host}/telegram-hook?token=${TOKEN}`
-  const isSet = await bot.api.setWebhook(webhookUrl)
+  const webhookUrl = `https://${host}/telegram-hook`
+  const success = await bot.api.setWebhook(webhookUrl)
   const info = await bot.api.getWebhookInfo()
-  return `Set webhook to ${webhookUrl.replaceAll(
-    TOKEN,
-    '*',
-  )}: ${isSet}<br/>${JSON.stringify(info).replaceAll(TOKEN, '*')}`
+  return {
+    success,
+    ...info,
+  }
 })

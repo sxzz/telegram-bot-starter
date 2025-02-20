df7433bc02b3ff201c1042d1a3ec009703c7f76c
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2025-02-20 22:35:10 +0800

    feat: remember token

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,19 @@
 import { TOKEN } from '~/src/env'
 
 export default defineEventHandler((event) => {
-  const query = getQuery(event)
-  if (query.token !== TOKEN) {
+  let { token } = getQuery(event)
+  token ||= getCookie(event, 'token')
+
+  if (!token || token !== TOKEN) {
     throw createError({
       status: 401,
       statusMessage: 'Unauthorized',
       message: 'Invalid token',
     })
   }
+
+  setCookie(event, 'token', TOKEN, {
+    httpOnly: true,
+    maxAge: 86400,
+  })
 })
